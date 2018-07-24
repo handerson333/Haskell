1e0d3d4804e0d118b28055531af67f48aff51156
hayden <anderrob@oregonstate.edu>
2018-07-23 22:20:49 -0700

    did all of test and stmt up to Iterate in Karel

--- Karel/KarelSemantics.hs
+++ Karel/KarelSemantics.hs
@@ -9,11 +9,13 @@ import KarelState
 
 -- | Valuation function for Test.
 test :: Test -> World -> Robot -> Bool
-test (Not t)      w r = undefined
-test (Faceing c)  w r = undefined
-test (Clear d)    w r = undefined
-test (Beeper)     w r = undefined
-test (Empty)      w r = undefined
+test (Not t)      w r = not (test t w r)                                -- Not test
+test (Facing c)   w r = c == (getFacing r)                              -- Facing Cardinal Direction
+test (Clear d)    w r = isClear (neighbor (getFacing r) (getPos r)) w   -- Clear Dir                
+test (Beeper)     w r = hasBeeper (getPos r) w                          -- Current location has a beeper?
+test (Empty)      w r = isEmpty r
+
+
 
 -- | Valuation function for Stmt.
 stmt :: Stmt -> Defs -> World -> Robot -> Result
@@ -22,7 +24,25 @@ stmt PickBeeper _ w r = let p = getPos r
                         in if hasBeeper p w
                               then OK (decBeeper p w) (incBag r)
                               else Error ("No beeper to pick at: " ++ show p)
-stmt _ _ _ _ = undefined
+stmt Move               _ w r =     if (test (Clear Front) w r) 
+                                    then OK w (updatePos (neighbor (getFacing r)) r) 
+                                    else Error "That direction is blocked"
+stmt PutBeeper          _ w r =     if not (isEmpty r) 
+                                    then OK (incBeeper (relativePos Front r) w) (decBag r)   
+                                    else Error "You dont have any beepers to place"                        --if bag is not empty, put beeper ahead and decrement bags beeper, else error
+stmt (Turn d)           _ w r = OK w (updateFacing (cardTurn d) r)
+stmt (Call m)           d w r = case lookup m d of
+                                    Just s      -> stmt s d w r
+                                    _           -> Error ("Couldn't do" ++ m)
+stmt (Iterate i s)      d w r = if i > 1 then case stmt s d w r of
+                                                Done  r'    -> Done r'
+                                                OK    w' r' -> stmt (Iterate (i-1) s) d w' r'                       
+                                                Error e     -> Error e
+                                    else stmt s d w r       
+stmt (If t s1 s2)       d w r = undefined
+stmt (While t s)        d w r = undefined
+stmt (Block (x:xs))     d w r = undefined
+
     
 -- | Run a Karel program.
 prog :: Prog -> World -> Robot -> Result
--- test.hs
+++ test.hs
@@ -333,3 +333,35 @@ countTrues = foldr
 
 
        
+ex13 = ('a' == 'a')
+ex14 = (16 /= 3)
+ex15 = (5 > 3) && ('p' <= 'q')
+ex16 = "Haskell" > "C++"
+
+foo :: Integer -> Integer
+foo 0 = 16
+foo 1
+  | "Haskell" > "C++" = 3
+  | otherwise         = 4
+foo n
+  | n < 0           = 0
+  | n `mod` 17 == 2 = -43
+  | otherwise       = n + 3
+  
+-- /show
+hailstone :: Integer -> Integer
+hailstone n
+  | n `mod` 2 == 0 = n `div` 2
+  | otherwise      = 3 * n + 1
+  
+-- show
+hailstoneSeq :: Integer -> [Integer]
+hailstoneSeq 1 = [1]
+hailstoneSeq n = n : hailstoneSeq (hailstone n)
+
+intListLength :: [Integer] -> Integer
+intListLength [] = 0
+intListLength (x:xs) = 1 + intListLength xs
+
+hailstoneLen :: Integer -> Integer
+hailstoneLen n = intListLength (hailstoneSeq n) - 1
